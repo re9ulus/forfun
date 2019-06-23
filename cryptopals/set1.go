ecb79026b979e0851eadccb53a4d24d1eb8ce4e1
Vasiliy Zemlyanov <re9ulusv@gmail.com>
2019-06-23 14:53:14 +0300

    Create preset for task2

--- cryptopals/set1.go
+++ cryptopals/set1.go
@@ -5,6 +5,7 @@ import (
 )
 
 
+// Convert hex to base64
 func task1(isVerbose bool) {
     fmt.Println("Task 1...")
     hexInput := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
@@ -21,9 +22,24 @@ func task1(isVerbose bool) {
 }
 
 
+// Fixed xor
+func task2(isVerbose bool) {
+    fmt.Println("Task 2...")
+    a := "1c0111001f010100061a024b53535009181c"
+    b := "686974207468652062756c6c277320657965"
+    expected := "746865206b696420646f6e277420706c6179"
+    actual := a + b  // XorString(a, b)
+    if actual != expected {
+        panic("Set 2 Task 2 failed")
+    }
+    fmt.Println("Task 2 Done.")
+}
+
+
 func main() {
     isVerbose := true
-    fmt.Println("Set 1...  ")
+    fmt.Println("Set 1...")
     task1(isVerbose)
+    task2(isVerbose)
     fmt.Println("Set 1 Done.")
 }
