8ad0c09f37373757b563582d5d07a37cf8fa32a0
Vasiliy Zemlyanov <re9ulusv@gmail.com>
2019-06-23 15:46:55 +0300

    Add set1 task2 solution, refactor utils

--- cryptopals/crypto_utils.go
+++ cryptopals/crypto_utils.go
@@ -6,29 +6,51 @@ import (
 )
 
 
-func BinHexToBase64(binHex []byte) []byte {
+func BinToBase64(binHex []byte) []byte {
     base64dst := make([]byte, base64.StdEncoding.EncodedLen(len(binHex)))
     base64.StdEncoding.Encode(base64dst, binHex)
     return base64dst
 }
 
 
-func StrHexToBinHex(strHex string) ([]byte, error) {
+func HexToBin(strHex string) []byte {
     src := []byte(strHex)
     dst := make([]byte, hex.DecodedLen(len(src)))
     _, err := hex.Decode(dst, src)
     if err != nil {
-        return nil, err
+        panic("HexToBin failed")
     }
-    return dst, err
+    return dst
 }
 
 
-func StrHexToBase64(strHex string) (string, error) {
-    binHex, err := StrHexToBinHex(strHex)
-    if err != nil {
-        return "", err
+func HexToBinBase64(strHex string) []byte {
+    return BinToBase64(HexToBin(strHex))
+}
+
+
+func HexToBase64(strHex string) string {
+    return string(HexToBinBase64(strHex))
+}
+
+
+func XorBinary(a, b []byte) []byte {
+    if len(a) != len(b) {
+        panic("Binary arrays must have same length")
     }
-    base64dst := BinHexToBase64(binHex)
-    return string(base64dst), nil
+    res := make([]byte, len(a))
+    for idx, _ := range a {
+        res[idx] = a[idx] ^ b[idx]
+    }
+    return res
+
+}
+
+
+func XorStrings(a, b string) string {
+    if len(a) != len(b) {
+        panic("String length must be equal")
+    }
+    binResBase64 := XorBinary(HexToBin(a), HexToBin(b))
+    return hex.EncodeToString(binResBase64)
 }
--- cryptopals/set1.go
+++ cryptopals/set1.go
@@ -11,7 +11,7 @@ func task1(isVerbose bool) {
     hexInput := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
     base64Expected := "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
 
-    base64Real, _ := StrHexToBase64(hexInput)
+    base64Real := HexToBase64(hexInput)
     if base64Expected != base64Real {
         panic("Set 1 Tasl 1 failed")
     }
@@ -28,7 +28,8 @@ func task2(isVerbose bool) {
     a := "1c0111001f010100061a024b53535009181c"
     b := "686974207468652062756c6c277320657965"
     expected := "746865206b696420646f6e277420706c6179"
-    actual := a + b  // XorString(a, b)
+    actual := XorStrings(a, b)
+    fmt.Println(actual)
     if actual != expected {
         panic("Set 2 Task 2 failed")
     }
