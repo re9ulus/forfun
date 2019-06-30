cdc75df529856972df6943715ce900f6ec7bbcd3
r9 <angeldaren@gmail.com>
2019-06-30 20:45:40 +0300

    Add task description to cryptopals task1

--- cryptopals/rust/src/main.rs
+++ cryptopals/rust/src/main.rs
@@ -3,6 +3,7 @@ extern crate hex;
 use std::str;
 
 
+// Convert hex to base64
 fn task1(is_verbose: bool) {
     let hex64line: &'static str = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
     let base64expected: &'static str ="SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
