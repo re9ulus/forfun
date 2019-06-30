3ba4e97c3ef39dd9c4d6b3f37b633d6fd863c03d
r9 <angeldaren@gmail.com>
2019-06-30 20:20:05 +0300

    Add cryptopals task1 solution on rust

---
+++ cryptopals/rust/src/main.rs
@@ -0,0 +1,23 @@
+extern crate base64;
+extern crate hex;
+use std::str;
+
+
+fn task1(is_verbose: bool) {
+    let hex64line: &'static str = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    let base64expected: &'static str ="SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+
+    let bytes = hex::decode(&hex64line).unwrap();
+    let base64actual = base64::encode(&bytes);
+
+    assert_eq!(base64expected, base64actual);
+    if is_verbose {
+        println!("Task 1 ans: {}", base64actual);
+    }
+}
+
+
+fn main() {
+    let is_verbose = true;
+    task1(is_verbose);
+}
