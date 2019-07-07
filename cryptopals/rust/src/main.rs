add213af9953dcec91d70378f425f3b18c97d8c1
r9 <angeldaren@gmail.com>
2019-07-07 12:27:21 +0300

    Add cryptopals task3 solution in rust

--- cryptopals/rust/src/main.rs
+++ cryptopals/rust/src/main.rs
@@ -1,10 +1,12 @@
 mod utils;
 mod s1t1;
 mod s1t2;
+mod s1t3;
 
 
 fn main() {
     let is_verbose = true;
     s1t1::solution(is_verbose);
     s1t2::solution(is_verbose);
+    s1t3::solution(is_verbose);
 }
--- cryptopals/rust/src/s1t3.rs
+++ cryptopals/rust/src/s1t3.rs
@@ -1,30 +1,34 @@
 extern crate hex;
+use std::str;
 use std::collections::HashMap;
 use std::fs;
-use crate::utils;
 
 
 type Vocab = HashMap<char, usize>;
 type Descriptor = Vec<usize>;
-// Create descriptor class/typedef?
-
-// TODO: Read file, create descriptor, compare with encoded line descriptor
 
 
-// fn cosineDistance(a Vec<u8>, b Vec<u8>) f64 {
-// 
-// }
-// 
-// 
-// fn xorWithChar(bytes &[u8]bool, ch u8) -> Vec<u8> {
-//     let mut res = vec![0; bytes.len()];
-//     for idx in 0..bytes.len() {
-//         res[idx] = bytes[idx] ^ ch;
-//     }
-//     return res;
-// }
-// 
-// 
+fn cosine_distance(a: &Descriptor, b: &Descriptor) -> f64 {
+    let mut enumerator = 0f64;
+    let mut sum_a = 0f64;
+    let mut sum_b = 0f64;
+    for idx in 0..a.len() {
+        enumerator += (a[idx] as f64) * (b[idx] as f64);
+        sum_a += (a[idx] as f64).powi(2);
+        sum_b += (b[idx] as f64).powi(2);
+    }
+    let denominator = sum_a.sqrt() * sum_b.sqrt();
+    enumerator / (denominator + 1e-7)
+}
+
+fn xor_with_char(bytes: &[u8], ch: &u8) -> Vec<u8> {
+    let mut res = vec![0; bytes.len()];
+    for idx in 0..bytes.len() {
+        res[idx] = bytes[idx] ^ ch;
+    }
+    res
+}
+
 fn build_descriptor(line: &str, vocab: &Vocab) -> Descriptor { 
     let mut descriptor: Descriptor = vec![0; vocab.len()];
     for ch in line.chars() {
@@ -32,7 +36,7 @@ fn build_descriptor(line: &str, vocab: &Vocab) -> Descriptor {
             descriptor[vocab[&ch]] += 1;
         }
     }
-    return descriptor;
+    descriptor
 }
 
 fn build_vocab(text: &str) -> Vocab {
@@ -44,10 +48,11 @@ fn build_vocab(text: &str) -> Vocab {
             vocab_size += 1;
         }
     }
-    return vocab;
+    vocab
 }
 
 
+#[allow(dead_code)]
 fn print_vocab(vocab: &Vocab) {
     for (key, val) in vocab {
         println!("{} {}", key, val);
@@ -55,6 +60,7 @@ fn print_vocab(vocab: &Vocab) {
 }
 
 
+#[allow(dead_code)]
 fn print_descriptor(descriptor: &Descriptor) {
     for val in descriptor {
         print!("{} ", val);
@@ -66,19 +72,30 @@ fn print_descriptor(descriptor: &Descriptor) {
 // Single-byte XOR cipher
 pub fn solution(is_verbose: bool) {
     let encoded_hex: &'static str = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+    let binary_input = hex::decode(&encoded_hex).unwrap();
 
     let text = fs::read_to_string("/home/r9/dev/forfun/cryptopals/rust/data/dracula.txt").unwrap();
 
     let vocab = build_vocab(&text);
-    print_vocab(&vocab);
     let lang_descriptor = build_descriptor(&text, &vocab);
-    print_descriptor(&lang_descriptor);
 
-    for ch in "abcdefghijklmnopqrstuvwxyzABCDEFGIJKLMNOPQRSTUVWXYZ".chars() {
-        // TODO: Xor with encoded_hex, convert to string, build descriptor, compute similliarity
-        // with lang_descriptor (cos dist)
+    let mut best_similiarity = -1f64;
+    let mut best_candidate = String::from("");
 
+    for ch in "abcdefghijklmnopqrstuvwxyzABCDEFGIJKLMNOPQRSTUVWXYZ1234567890".as_bytes() {
+        let candidate = xor_with_char(&binary_input, ch);
+        let str_repr = String::from_utf8(candidate).unwrap();
+        let candidate_descriptor = build_descriptor(&str_repr, &vocab);
+        let similiarity = cosine_distance(&lang_descriptor, &candidate_descriptor);
+        if best_similiarity < similiarity {
+            best_similiarity = similiarity;
+            best_candidate = str_repr;
+        }
     }
-    println!("Done");
+    
+    assert_eq!(best_candidate, "Cooking MC's like a pound of bacon");
+    if is_verbose {
+        println!("Task 3 ans: Msg: `{}`; Similiarity: {}", best_candidate, best_similiarity);
+    }
 }
 
