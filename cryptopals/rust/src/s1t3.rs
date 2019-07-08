8463e03e9e056c0e51ab7e63e9072f85a22c8612
r9 <angeldaren@gmail.com>
2019-07-08 22:49:09 +0300

    Refactor, move funcs to utils

--- cryptopals/rust/src/s1t3.rs
+++ cryptopals/rust/src/s1t3.rs
@@ -1,72 +1,7 @@
 extern crate hex;
+use crate::utils;
 use std::str;
-use std::collections::HashMap;
 use std::fs;
-
-
-type Vocab = HashMap<char, usize>;
-type Descriptor = Vec<usize>;
-
-
-fn cosine_distance(a: &Descriptor, b: &Descriptor) -> f64 {
-    let mut enumerator = 0f64;
-    let mut sum_a = 0f64;
-    let mut sum_b = 0f64;
-    for idx in 0..a.len() {
-        enumerator += (a[idx] as f64) * (b[idx] as f64);
-        sum_a += (a[idx] as f64).powi(2);
-        sum_b += (b[idx] as f64).powi(2);
-    }
-    let denominator = sum_a.sqrt() * sum_b.sqrt();
-    enumerator / (denominator + 1e-7)
-}
-
-fn xor_with_char(bytes: &[u8], ch: &u8) -> Vec<u8> {
-    let mut res = vec![0; bytes.len()];
-    for idx in 0..bytes.len() {
-        res[idx] = bytes[idx] ^ ch;
-    }
-    res
-}
-
-fn build_descriptor(line: &str, vocab: &Vocab) -> Descriptor { 
-    let mut descriptor: Descriptor = vec![0; vocab.len()];
-    for ch in line.chars() {
-        if vocab.contains_key(&ch) {
-            descriptor[vocab[&ch]] += 1;
-        }
-    }
-    descriptor
-}
-
-fn build_vocab(text: &str) -> Vocab {
-    let mut vocab = HashMap::new();
-    let mut vocab_size = 0;
-    for ch in text.chars() {
-        if !vocab.contains_key(&ch) {
-            vocab.insert(ch, vocab_size);
-            vocab_size += 1;
-        }
-    }
-    vocab
-}
-
-
-#[allow(dead_code)]
-fn print_vocab(vocab: &Vocab) {
-    for (key, val) in vocab {
-        println!("{} {}", key, val);
-    }
-}
-
-
-#[allow(dead_code)]
-fn print_descriptor(descriptor: &Descriptor) {
-    for val in descriptor {
-        print!("{} ", val);
-    }
-    println!()
-}
 
 
 // Single-byte XOR cipher
@@ -76,17 +11,17 @@ pub fn solution(is_verbose: bool) {
 
     let text = fs::read_to_string("/home/r9/dev/forfun/cryptopals/rust/data/dracula.txt").unwrap();
 
-    let vocab = build_vocab(&text);
-    let lang_descriptor = build_descriptor(&text, &vocab);
+    let vocab = utils::build_vocab(&text);
+    let lang_descriptor = utils::build_descriptor(&text, &vocab);
 
     let mut best_similiarity = -1f64;
     let mut best_candidate = String::from("");
 
     for ch in "abcdefghijklmnopqrstuvwxyzABCDEFGIJKLMNOPQRSTUVWXYZ1234567890".as_bytes() {
-        let candidate = xor_with_char(&binary_input, ch);
+        let candidate = utils::xor_with_char(&binary_input, ch);
         let str_repr = String::from_utf8(candidate).unwrap();
-        let candidate_descriptor = build_descriptor(&str_repr, &vocab);
-        let similiarity = cosine_distance(&lang_descriptor, &candidate_descriptor);
+        let candidate_descriptor = utils::build_descriptor(&str_repr, &vocab);
+        let similiarity = utils::cosine_distance(&lang_descriptor, &candidate_descriptor);
         if best_similiarity < similiarity {
             best_similiarity = similiarity;
             best_candidate = str_repr;
--- cryptopals/rust/src/utils.rs
+++ cryptopals/rust/src/utils.rs
@@ -1,8 +1,79 @@
+use std::collections::HashMap;
+
+
+pub type Vocab = HashMap<char, usize>;
+pub type Descriptor = Vec<usize>;
+
+
 pub fn xor_bytes(bytes1: &[u8], bytes2: &[u8]) -> Vec<u8> {
     let mut res = vec![0; bytes1.len()];
     for idx in 0..bytes1.len() {
         res[idx] = bytes1[idx] ^ bytes2[idx];
     }
     return res;
+}
+
+
+pub fn xor_with_char(bytes: &[u8], ch: &u8) -> Vec<u8> {
+    let mut res = vec![0; bytes.len()];
+    for idx in 0..bytes.len() {
+        res[idx] = bytes[idx] ^ ch;
+    }
+    res
+}
+
+
+pub fn cosine_distance(a: &Descriptor, b: &Descriptor) -> f64 {
+    let mut enumerator = 0f64;
+    let mut sum_a = 0f64;
+    let mut sum_b = 0f64;
+    for idx in 0..a.len() {
+        enumerator += (a[idx] as f64) * (b[idx] as f64);
+        sum_a += (a[idx] as f64).powi(2);
+        sum_b += (b[idx] as f64).powi(2);
+    }
+    let denominator = sum_a.sqrt() * sum_b.sqrt();
+    enumerator / (denominator + 1e-7)
+}
+
+
+pub fn build_vocab(text: &str) -> Vocab {
+    let mut vocab = HashMap::new();
+    let mut vocab_size = 0;
+    for ch in text.chars() {
+        if !vocab.contains_key(&ch) {
+            vocab.insert(ch, vocab_size);
+            vocab_size += 1;
+        }
+    }
+    vocab
+}
+
+
+pub fn build_descriptor(line: &str, vocab: &Vocab) -> Descriptor { 
+    let mut descriptor: Descriptor = vec![0; vocab.len()];
+    for ch in line.chars() {
+        if vocab.contains_key(&ch) {
+            descriptor[vocab[&ch]] += 1;
+        }
+    }
+    descriptor
+}
+
+
+#[allow(dead_code)]
+fn print_vocab(vocab: &Vocab) {
+    for (key, val) in vocab {
+        println!("{} {}", key, val);
+    }
+}
+
+
+#[allow(dead_code)]
+fn print_descriptor(descriptor: &Descriptor) {
+    for val in descriptor {
+        print!("{} ", val);
+    }
+    println!()
 }
 
