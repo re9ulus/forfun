c9c67dbeda3575a3faaa76219d3b31c09f957d8d
r9 <angeldaren@gmail.com>
2019-07-10 22:32:08 +0300

    Add rust solution to cryptopals task4

---
+++ cryptopals/rust/src/s1t4.rs
@@ -0,0 +1,41 @@
+use std::fs;
+use crate::utils;
+
+// TODO: Refactor to decrease code duplication with task3
+
+// Detect single character xor
+pub fn solution(is_verbose: bool) {
+
+    let text = fs::read_to_string("/home/r9/dev/forfun/cryptopals/rust/data/dracula.txt").unwrap();
+    let vocab = utils::build_vocab(&text);
+    let lang_descriptor = utils::build_descriptor(&text, &vocab);
+
+    let input_file = "/home/r9/dev/forfun/cryptopals/rust/data/s1t4input.txt";
+    let input_text = fs::read_to_string(input_file).unwrap();
+
+    let mut best_similiarity = -1f64;
+    let mut best_candidate = String::from("");
+
+    for line in input_text.split("\n") {
+        if line.is_empty() {
+            continue;
+        }
+        println!("Line is {}", line);
+        let binary_line = hex::decode(&line).unwrap();
+        for ch in "abcdefghijklmnopqrstuvwxyzABCDEFGIJKLMNOPQRSTUVWXYZ1234567890".as_bytes() {
+            let candidate = utils::xor_with_char(&binary_line, ch);
+            let str_repr = String::from_utf8(candidate).unwrap_or("".to_string());
+            if str_repr.is_empty() {
+                continue;
+            }
+            let candidate_descriptor = utils::build_descriptor(&str_repr, &vocab);
+            let similiarity = utils::cosine_distance(&lang_descriptor, &candidate_descriptor);
+            // println!("{} {}", similiarity, str_repr);
+            if best_similiarity < similiarity {
+                best_similiarity = similiarity;
+                best_candidate = str_repr;
+            }
+        }
+    }
+    println!("BEST: {} {}", best_similiarity, best_candidate);
+}
